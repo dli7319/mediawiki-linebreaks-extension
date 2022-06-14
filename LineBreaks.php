cffb1238bc3ba7d1d719ffaf44969c32d2d81e03
David Li <dli7319@gmail.com>
2022-06-14 14:03:07 -0400

    Add support for mediawiki >=1.36.0

--- LineBreaks.php
+++ LineBreaks.php
@@ -1,12 +1,13 @@
 <?php
 class LineBreaks
 {
-    public static function onParserBeforeStrip(&$parser, &$text, &$strip_state)
+    public static function onParserBeforeInternalParse(&$parser, &$text, &$strip_state)
     {
-	global $wgLbUseBackslash;    
-	$text = str_replace("  \n", "<br />\n", $text);
-	if ($wgLbUseBackslash) {
-	  $text = str_replace("\\\n", "<br />\n", $text);
-	}
+		global $wgLbUseBackslash;    
+		$text = str_replace("  \n", "<br />\n", $text);
+		if ($wgLbUseBackslash) {
+			$text = str_replace("\\\n", "<br />\n", $text);
+		}
+		return true;
     }
 }
